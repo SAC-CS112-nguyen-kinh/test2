cc0cb917ec46ffcce109e8fdcb6b6a18eb2c562c
SAC-CS112-nguyen-kinh <kinhdinhnguyen12@yahoo.com>
2016-05-04 20:48:55 -0700

    Added files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+package test2;
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args)
+	{
+		int numberOfDice;
+		Scanner input = new Scanner(System.in);
+		Dices rollDice = new Dices();
+		
+	}
+
+}
